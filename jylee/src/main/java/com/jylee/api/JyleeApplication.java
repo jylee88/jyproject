11004e9f01bdc180bf0874e771275717671683fa
juicyj <juicyj0313@gmail.com>
2024-01-17 22:20:02 +0900

    git sts 연동

---
+++ jylee/src/main/java/com/jylee/api/JyleeApplication.java
@@ -0,0 +1,13 @@
+package com.jylee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyleeApplication.class, args);
+	}
+
+}
---
+++ jylee/src/test/java/com/jylee/api/JyleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jylee.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
